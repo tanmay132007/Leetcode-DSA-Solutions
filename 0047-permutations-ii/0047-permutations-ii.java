a82e26fbbbbc19f917d63efb11d1929646776c0e
tanmay rajput <tanmayrajput400@gmail.com>
2026-08-26 18:50:00 +0530

    Time: 4 ms (30.01%), Space: 47 MB (13.81%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public void fun(List<Integer> nums, List<Integer> temp) {
+
+        if (nums.size() == 0) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        Set<Integer> tried = new HashSet<>(); 
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (tried.contains(nums.get(i))) continue;
+
+            tried.add(nums.get(i));
+
+            List<Integer> ip = new ArrayList<>(nums);
+            List<Integer> op = new ArrayList<>(temp);
+
+            op.add(nums.get(i));
+            ip.remove(i);
+
+            fun(ip, op);
+        }
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        ans.clear();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : nums) {
+            list.add(num);
+        }
+
+        fun(list, new ArrayList<>());
+
+        return ans;
+    }
+}
